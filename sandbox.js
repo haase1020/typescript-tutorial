2542793f82b68781446e9edb405bf5dde2f6d65c
Mandi <haase1020@gmail.com>
2021-01-28 07:14:35 -0500

    change file structure

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,7 @@
-var character = "mario";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-inputs.forEach(function (input) {
-  console.log(input);
-});
+//
+var character;
+var age;
+var isLoggedIn;
+character = 'me';
+var ninja;
+ninja = [1, 2];
