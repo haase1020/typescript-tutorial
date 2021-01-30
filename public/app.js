ced02ecc3aa67472e378d0306ec7669face7e2da
Mandi <haase1020@gmail.com>
2021-01-30 14:14:09 -0500

    complete small project

--- public/app.js
+++ public/app.js
@@ -1,20 +1,24 @@
-"use strict";
-// classes
-var Invoice = /** @class */ (function () {
-    function Invoice(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+const form = document.querySelector('.new-item-form');
+console.log(form.children);
+// inputs
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
     }
-    Invoice.prototype.format = function () {
-        return this.client + " owes " + this.amount + "$ for " + this.details;
-    };
-    return Invoice;
-}());
-var invOne = new Invoice('mandi', 'work on testing', 250);
-console.log(invOne);
-var invoices = [];
-invoices.push(invOne);
-console.log(invoices);
-invOne.client = 'jon';
-console.log(invOne);
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
+});
--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,30 @@
-// classes
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
 
-class Invoice {
-    client: string;
-    details: string;
-    amount: number;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
 
-    constructor(c:string, d: string, a: number) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
-    format() {
-        return `${this.client} owes ${this.amount}$ for ${this.details}`;
-    }
-}
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
 
-const invOne = new Invoice('mandi', 'work on testing', 250);
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
 
-console.log(invOne)
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-console.log(invoices)
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
 
-invOne.client = 'jon'
-console.log(invOne)
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+  
+  list.render(doc, type.value, 'end');
+});
